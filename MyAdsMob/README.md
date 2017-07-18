8260930501da16eb21ddf18d56122c4344b67125
crestinfo <godfather.php@gmail.com>
2017-07-18 12:04:03 +0530

    Integration of Google AdMob

---
+++ MyAdsMob/README.md
@@ -0,0 +1,28 @@
+<h2>Integrating AdMob in Android.</h2>
+<p>The example is based on how to integrate the AdMob in Android.</4>
+
+<h2>Demonstration</h2>
+
+<h4>Guide.</h4> 
+<p>You can get the documentation of the AdMob from the official page of Google AdMob.<p>
+         
+
+
+
+<h4>Step 1 : You need to add the Ads Activity to your Manifest file.</h4> 
+<p><code><activity android:name="com.google.android.gms.ads.AdActivity"</code><p>
+<p><code>android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize"</code><p>
+<p><code>android:theme="@android:style/Theme.Translucent" /></code><p>
+
+<h4>Step 2 : Also the meta tag is rtequired for to integrate the google play services.</h4> 
+<p><code><meta-data android:name="com.google.android.gms.version"</code><p>
+<p><code>android:value="@integer/google_play_services_version" /></code><p>
+
+
+<h4>Output:</h4> 
+<p><code>You will soon get Advertisement in your device.</code></p>
+
+
+
+<p>If you need more help <a href="http://www.crestinfotech.com/contact-us/" target="_blank">contact us</a> 
+or email us on <a href="mailto:raxit4u2@gmail.com">raxit4u2@gmail.com</a></p>
---
+++ MyAdsMob/app/src/main/java/com/crest/myadsmob/MainActivity.java
@@ -0,0 +1,98 @@
+package com.crest.myadsmob;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+public class MainActivity extends Activity implements RewardedVideoAdListener {
+    // Remove the below line after defining your own ad unit ID.
+    private static final String TOAST_TEXT = "Test ads are being shown. "
+            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+
+    private static final int START_LEVEL = 1;
+    private int mLevel;
+    private Button mNextLevelButton;
+    private InterstitialAd mInterstitialAd;
+    private RewardedVideoAd rewardedVideoAd;
+    private TextView mLevelTextView;
+    static String AD_UNIT_ID = "ca-app-pub-3940256099942544/5224354917";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the next level button, which tries to show an interstitial when clicked.
+        mNextLevelButton = ((Button) findViewById(R.id.next_level_button));
+        // Use an activity context to get the rewarded video instance.
+        rewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        rewardedVideoAd.setRewardedVideoAdListener(this);
+
+        loadRewardedVideoAd();
+
+
+        mNextLevelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rewardedVideoAd.show();
+            }
+        });
+
+        // Create the text view to show the level number.
+        mLevelTextView = (TextView) findViewById(R.id.level);
+        mLevel = START_LEVEL;
+
+    }
+
+    void loadRewardedVideoAd() {
+        rewardedVideoAd.loadAd(AD_UNIT_ID, new AdRequest.Builder().build());
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewarded(RewardItem reward) {
+        Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount(), Toast.LENGTH_SHORT).show();
+        // Reward the user.
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "onRewardedVideoAdLeftApplication",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+    }
+}
